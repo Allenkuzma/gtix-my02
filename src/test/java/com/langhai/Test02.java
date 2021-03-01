649b3e34a64e72e6a5e2dcf9cfa67c353d5734b9
langhai <648798558@qq.com>
2021-03-01 19:31:35 +0800

    C1客户端更新操作dev节点

--- src/test/java/com/langhai/Test02.java
+++ src/test/java/com/langhai/Test02.java
@@ -3,5 +3,6 @@ package com.langhai;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("创建了一个新的分支！！！");
+        System.out.println("更新操作dev01");
     }
 }
